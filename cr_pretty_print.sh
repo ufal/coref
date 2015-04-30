989a2fa60c3f40577275c831124931efe5202273
Michal Novák <mixnov@gmail.com>
2015-04-30 09:09:53 +0000

    sorting the pretty prints to show the errors first
    Mon Apr 20 00:46:36 CEST 2015

--- cr_pretty_print.sh
+++ cr_pretty_print.sh
@@ -18,3 +18,13 @@ treex -p --jobs 50 -L$language -Sref \
     Util::Eval tnode='my @antes = $tnode->get_coref_nodes; $_->wild->{coref_diag}{sys_ante_for}{$tnode->id} = 1 foreach (@antes)' \
     Coref::PrettyPrint path="$tmp_dir/parts"
 find $tmp_dir/parts -name '*.txt' | sort | xargs cat > $tmp_dir/all.txt
+cat $tmp_dir/all.txt | \
+    tr '\n' '\t' | \
+    sed 's/\t\t/\n/g' | \
+    sort -k3,3 -t'	' | \
+    sed 's/$/\n/' | \
+    sed 's/ERR:\t/ERR: /g' | \
+    sed 's/OK:\t/OK: /g' | \
+    sed 's/\t/\n/g' | \
+    sed 's/ERR: /ERR:\t/g' | \
+    sed 's/OK: /OK:\t/g' > $tmp_dir/all.sorted.txt
