ea7361ee0911909a178cb7c5459f8d284bacd39b
Michal Novák <mixnov@gmail.com>
2015-04-30 09:09:55 +0000

    rather print out the coreference links on the src data
    Mon Apr 20 17:29:36 CEST 2015

--- cr_pretty_print.sh
+++ cr_pretty_print.sh
@@ -6,16 +6,18 @@ data_list=$2
 tmp_dir=tmp/cr_pretty_print/`date +%Y-%m-%d_%H-%M-%S`_$$
 mkdir -p $tmp_dir
 
-#    Write::Treex path="$tmp_dir/trees"
-#mkdir -p $tmp_dir/trees
+mkdir -p $tmp_dir/trees
+mkdir -p $tmp_dir/trees.del
 
 mkdir -p $tmp_dir/parts
-treex -p --jobs 50 -L$language -Sref \
+treex -p --jobs 50 -L$language -Ssrc \
     Read::Treex from=@$data_list \
-    Util::Eval tnode='my @antes = $tnode->get_coref_chain; $_->wild->{coref_diag}{key_ante_for}{$tnode->id} = 1 foreach (@antes)' \
+    Util::Eval tnode='if ($tnode->wild->{coref_diag}{is_anaph}) { my @antes = $tnode->get_coref_nodes; $_->wild->{coref_diag}{sys_ante_for}{$tnode->id} = 1 foreach (@antes) }' \
     Coref::RemoveLinks \
-    T2T::CopyCorefFromAlignment selector=src \
-    Util::Eval tnode='my @antes = $tnode->get_coref_nodes; $_->wild->{coref_diag}{sys_ante_for}{$tnode->id} = 1 foreach (@antes)' \
+    Write::Treex path="$tmp_dir/trees.del" \
+    T2T::CopyCorefFromAlignment selector=ref \
+    Util::Eval tnode='my @antes = $tnode->get_coref_chain; $_->wild->{coref_diag}{key_ante_for}{$tnode->id} = 1 foreach (@antes)' \
+    Write::Treex path="$tmp_dir/trees" \
     Coref::PrettyPrint path="$tmp_dir/parts"
 find $tmp_dir/parts -name '*.txt' | sort | xargs cat > $tmp_dir/all.txt
 cat $tmp_dir/all.txt | \
@@ -28,3 +30,6 @@ cat $tmp_dir/all.txt | \
     sed 's/\t/\n/g' | \
     sed 's/ERR: /ERR:\t/g' | \
     sed 's/OK: /OK:\t/g' > $tmp_dir/all.sorted.txt
+
+echo "All coref data pretty printed in: $tmp_dir/all.txt"
+echo "Sorted with the errors first: $tmp_dir/all.sorted.txt"
