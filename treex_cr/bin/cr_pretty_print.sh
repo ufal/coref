8f5444b1e7a0d3cf0b81c9145aad75a2e39e92ee
Michal Novak <mnovak@ufal.mff.cuni.cz>
2016-04-08 15:47:06 +0200

    pretty print to find the issues easily added to treex_cr

--- treex_cr/bin/cr_pretty_print.sh
+++ treex_cr/bin/cr_pretty_print.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 language=$1
-data_list=$2
-d=$3
+data=$2
+tmp_dir=$3
 
-d_no_space=`echo $d | sed 's/ /_/g'`
-last_n=`find tmp/cr_pretty_print -maxdepth 1 -mindepth 1 -path "*/run-*" -type d | cut -f3 -d'/' | cut -f1 -d'_' | cut -f2 -d'-' | sort | tail -n1` || 001
-next_n=`perl -e 'printf "%03d", $ARGV[0]+1' $last_n`
-
-tmp_dir=tmp/cr_pretty_print/run-$next_n'_'`date +%Y-%m-%d_%H-%M-%S`'_'$$'_'$d_no_space
-mkdir -p $tmp_dir
+#last_n=`find tmp/cr_pretty_print -maxdepth 1 -mindepth 1 -path "*/run-*" -type d | cut -f3 -d'/' | cut -f1 -d'_' | cut -f2 -d'-' | sort | tail -n1` || 001
+#next_n=`perl -e 'printf "%03d", $ARGV[0]+1' $last_n`
+#tmp_dir=tmp/cr_pretty_print/run-$next_n'_'`date +%Y-%m-%d_%H-%M-%S`'_'$$'_'$d_no_space
+#mkdir -p $tmp_dir
 
 mkdir -p $tmp_dir/trees
 mkdir -p $tmp_dir/trees.del
-
 mkdir -p $tmp_dir/parts
-treex -p --jobs 50 -L$language -Ssrc \
-    Read::Treex from=@$data_list \
+
+treex -p --jobs 50 --workdir=$tmp_dir/run -L$language -Ssrc \
+    Read::Treex from=\'$data\' \
     Util::Eval tnode='if ($tnode->wild->{coref_diag}{is_anaph}) { my @antes = $tnode->get_coref_nodes; $_->wild->{coref_diag}{sys_ante_for}{$tnode->id} = 1 foreach (@antes) }' \
     Coref::RemoveLinks \
     Write::Treex path="$tmp_dir/trees.del" \
